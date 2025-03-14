a432822e157eddc5343fb22fdbb0de2c76babcc6
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2025-03-14 12:26:30 +0200

    moved redo.rds to redo

---
+++ R/redo.r
@@ -0,0 +1,29 @@
+#' Rebuild and Read files
+#'
+#' Uses "redo" build system to check if the specified file is out of date,
+#' rebuilds it if necessary and then tries to read the file.
+#'
+#' Technically the function first invokes a "redo-ifchange" system command
+#' and then reads the specified file using the appropriate R function.
+#' For an example of "redo" implementation see \href{http://www.goredo.cypherpunks.su}.
+#'
+#' List of currently supported file types: .rds, .csv.
+#'
+#' @param file the name of the file which the data are to be read from.
+#' @param ... optional arguments passed to the function reading the file.
+#'
+#' @return an R object.
+#'
+#' @author Karolis Koncevičius
+#' @export
+redo <- function(file, ...) {
+  system(paste("redo-ifchange", file))
+  ext <- tolower(sub(".*\\.", "", file))
+  if(ext == "rds") {
+    readRDS(file, ...)
+  } else if(ext == "csv") {
+    read.csv(file, ...)
+  } else {
+    stop("file type: '", ext, "' is not supported")
+  }
+}
