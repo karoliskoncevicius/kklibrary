602455decaf3766a522539b3537f8011aac6f078
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2025-03-18 12:51:46 +0200

    added cell2col

---
+++ R/cell2col.r
@@ -0,0 +1,44 @@
+#' Convert Cell Type Names to Colors
+#'
+#' Converts cell type names to colors.
+#' Currently the color palette is hard-coded within the function.
+#' Black color is returned for undefined an missing cell types.
+#'
+#' @param x a vector of cell type names.
+#'
+#' @return a vector of colors for each element in x
+#'
+#' @examples
+#' cells <- c("blood", "pbmc", "bcell", "bn", "neu", "mix", "mono")
+#' cell2col(cells)
+#'
+#' @author Karolis Koncevičius
+#' @export
+cell2col <- function(x) {
+  cols <- c(blood = "tomato3",
+            wb    = "tomato3",
+            pbmc  = "salmon2",
+            gran  = "darkorange1",
+            neu   = "#FAA43A",
+            mono  = "#DECF3F",
+            eos   = "#FFF804",
+            baso  = "#C4A484",
+            nk    = "#B276B2",
+            tcell = "blue",
+            tcd4  = "dodgerblue2",
+            tcd4n = "deepskyblue",
+            tcd4m = "dodgerblue3",
+            tcd8  = "skyblue3",
+            tcd8n = "lightblue2",
+            tcd8m = "steelblue4",
+            treg  = "cyan4",
+            bcell = "green3",
+            bn    = "lawngreen",
+            bm    = "forestgreen"
+            )
+
+  x <- tolower(x)
+  x <- setNames(cols[x], x)
+  x[is.na(x)] <- "black"
+  x
+}
